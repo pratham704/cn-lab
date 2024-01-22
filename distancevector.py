94ae5f68244f4649838994da00446812440b75e5
prathamfindmyoptions <153301707+prathamfindmyoptions@users.noreply.github.com>
2024-01-22 20:33:18 +0530

    Refactor distancevector.py to use NetworkX library for graph operations

--- distancevector.py
+++ distancevector.py
@@ -1,77 +1,34 @@
-import sys
+import networkx as nx
 
+def add_link(graph, node1, node2, cost):
+    graph.add_edge(node1, node2, weight=cost)
 
-#make me understand the whole code with iterations
-
-
-
-
-
-
-
-
-
-class Graph:
-	def __init__(self, vertices):
-		self.V = vertices
-		self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
-
-	def min_distance(self, dist, spt_set):
-		min_dist = sys.maxsize
-		min_index = -1
-
-		for v in range(self.V):
-			if dist[v] < min_dist and spt_set[v] == False:
-				min_dist = dist[v]
-				min_index = v
-
-		return min_index
-
-	def dijkstra(self, src):
-		dist = [sys.maxsize] * self.V
-		dist[src] = 0
-		spt_set = [False] * self.V
-
-		for _ in range(self.V):
-			u = self.min_distance(dist, spt_set)
-			spt_set[u] = True
-
-			for v in range(self.V):
-				if (
-					self.graph[u][v] > 0
-					and spt_set[v] == False
-					and dist[v] > dist[u] + self.graph[u][v]
-				):
-					dist[v] = dist[u] + self.graph[u][v]
-
-		print("Vertex \t Distance from Source")
-		for node in range(self.V):
-			print(f"{node} \t\t {dist[node]}")
+def print_routing_table(graph, nodes, source):
+    print(f"Routing table for Node {source}:")
+    print("Destination\tCost")
+    distances = nx.single_source_dijkstra_path_length(graph, source, weight='weight')
+    for dest in nodes:
+        cost = distances.get(dest, float('inf'))
+        print(f"{dest}\t\t{cost}")
 
 # Example usage
-g = Graph(9)
-g.graph = [
-	[0, 4, 0, 0, 0, 0, 0, 8, 0],
-	[4, 0, 8, 0, 0, 0, 0, 11, 0],
-	[0, 8, 0, 7, 0, 4, 0, 0, 2],
-	[0, 0, 7, 0, 9, 14, 0, 0, 0],
-	[0, 0, 0, 9, 0, 10, 0, 0, 0],
-	[0, 0, 4, 14, 10, 0, 2, 0, 0],
-	[0, 0, 0, 0, 0, 2, 0, 1, 6],
-	[8, 11, 0, 0, 0, 0, 1, 0, 7],
-	[0, 0, 2, 0, 0, 0, 6, 7, 0],
-]
+nodes = [1, 2, 3, 4, 5]
+graph = nx.Graph()
 
-g.dijkstra(0)
+add_link(graph, 1, 2, 2)
+add_link(graph, 1, 3, 2)
+add_link(graph, 1, 4, 1)
+add_link(graph, 2, 3, 3)
+add_link(graph, 2, 1, 2)
+add_link(graph, 2, 5, 1)
+add_link(graph, 3, 1, 2)
+add_link(graph, 3, 4, 4)
+add_link(graph, 3, 2, 3)
+add_link(graph, 3, 5, 1)
+add_link(graph, 4, 1, 1)
+add_link(graph, 4, 3, 4)
+add_link(graph, 5, 2, 1)
+add_link(graph, 5, 3, 1)
 
-
-
-
-
-
-
-
-
-
-
-
+for node in nodes:
+    print_routing_table(graph, nodes, node)
