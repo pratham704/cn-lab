9cd91ec82dc7de881f6baaea84dd34cef8258346
prathamfindmyoptions <153301707+prathamfindmyoptions@users.noreply.github.com>
2024-01-22 19:58:53 +0530

    Refactor RSA key generation and encryption

--- Rsa.py
+++ Rsa.py
@@ -1,40 +1,23 @@
-import random
-from sympy import isprime, mod_inverse, gcd
+from sympy import randprime, mod_inverse
 
-def generate_prime(bits):
-    while True:
-        num = random.getrandbits(bits)
-        if isprime(num):
-            return num
-
-# Function to generate RSA key pairs using sympy
 def generate_key_pair(bits):
-    p = generate_prime(bits)
-    q = generate_prime(bits)
+    p = randprime(2**(bits-1), 2**bits)
+    q = randprime(2**(bits-1), 2**bits)
     n = p * q
     phi = (p - 1) * (q - 1)
-    
-    while True:
-        e = random.randint(2, phi - 1)
-        if gcd(e, phi) == 1:
-            break
-            
+    e = randprime(2, phi - 1)
     d = mod_inverse(e, phi)
-    public_key = (n, e)
-    private_key = (n, d)
-    
-    return public_key, private_key
+    return (n, e), (n, d)
 
 # Function to encrypt a message
 def encrypt(public_key, message):
     n, e = public_key
     return [pow(ord(char), e, n) for char in message]
 
-
-# Function to decrypt a message
 def decrypt(private_key, cipher_text):
     n, d = private_key
-    return  ''.join([chr(pow(char, d, n)) for char in cipher_text])
+    return ''.join(chr(pow(char, d, n)) for char in cipher_text)
+
 
 if __name__ == "__main__":
     bits = 8  
