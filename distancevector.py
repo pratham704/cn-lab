7449e6f61f08b664595c81aed811a56067fd1f88
Pratham Siddannavar <125141594+pratham704@users.noreply.github.com>
2024-01-23 11:43:54 +0530

    Update distancevector.py

--- distancevector.py
+++ distancevector.py
@@ -3,7 +3,7 @@ import networkx as nx
 def add_link(graph, start, end, cost):
     graph.add_edge(start, end, weight=cost)
 
-nodes = [1, 2, 3, 4]
+nodes = [1, 2, 3, 4, 5]
 graph = nx.Graph()
 
 add_link(graph, 1, 2, 2)
@@ -12,15 +12,17 @@ add_link(graph, 1, 4, 1)
 add_link(graph, 2, 3, 3)
 add_link(graph, 2, 1, 2)
 add_link(graph, 3, 1, 2)
+add_link(graph, 3, 2, 3)
 add_link(graph, 3, 4, 4)
 add_link(graph, 4, 1, 1)
+add_link(graph, 4, 3, 4)
+add_link(graph, 5, 3, 1)  # node 5 which is only conneted to node 3 
 
-
-for source in nodes:
-    print(f"Routing table for Node {source}:")
-    print("Destination\tCost")
-    distances = nx.single_source_dijkstra_path_length(graph, source, weight='weight')
-    for dest in nodes:
-        cost = distances.get(dest, float('inf'))
-        print(f"{dest}\t\t{cost}")
+for node in nodes:
+    print(f"Routing table for Node {node}:")
+    print("dest\tCost")
+    distances = nx.single_source_dijkstra_path_length(graph, node, weight='weight')
+    for destination in nodes:
+        cost = distances.get(destination, float('inf'))
+        print(f"{destination}\t{cost}")
     print()
