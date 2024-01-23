4915f0639f714c2de8376a4a866e3ffd1c41fb84
Pratham Siddannavar <125141594+pratham704@users.noreply.github.com>
2024-01-23 11:52:06 +0530

    Update leakybucket.py

--- leakybucket.py
+++ leakybucket.py
@@ -1,27 +1,33 @@
 from time import sleep
 
-
-def leakyBucket(bucket_size, packets, rate):
-    cycle = bucket = 0
-    for p in packets:
-        if bucket+p > bucket_size:
-            bucket = 0
-            cycle += 1
-            print(f"Dropped {p}\nCycle {cycle} complete\n")
-            sleep(1/rate)
+def leaky_bucket(packets,bucket_size,rate):
+    bucket=0
+    cycle=0
+    
+    for p in packets :
+        
+        if bucket+p>bucket_size:
+           bucket=0
+           cycle+=1
+           print(f"Dropped {p}\tcycle {cycle} complete")
+           sleep(1/rate)
+           
+           
         else:
-            bucket += p
-            print(f"Sent {p}\t")
-            
-            
-            
 
-
+            
+            bucket+=p
+            print(f"Packet sent {p}")
+            
+            
+            
 packets = [2, 3, 5, 6, 7, 9, 2, 8, 10, 11, 2, 4, 7, 5, 8, 9, 1, 3, 12]
 bucket_size = 10
 rate = 2
-leakyBucket(bucket_size, packets, rate) 
-
-
-
-
+leaky_bucket(packets,bucket_size , rate)            
+        
+        
+        
+    
+    
+    
